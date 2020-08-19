9d5b1ce50a720b680530a942b19f62f1e3414878
hieudicodedao <xu.viethieu@gmail.com>
2020-08-19 21:27:03 +0700

    first feture commit

---
+++ feture2.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class feture2 extends Component {
+    render() {
+        return (
+            <div>
+                <p>asdjfldsajflkjdsafkjdsafjdsaf</p>
+            </div>
+        )
+    }
+}
